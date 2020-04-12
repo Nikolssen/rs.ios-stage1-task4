4bca82f8b5e91d9cbb3aa1dc37c3a328db736fe6
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-04-12 10:10:17 +0300

    Update TelephoneFinder.m

--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -2,7 +2,34 @@
 
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+    NSDictionary *numberDict = @{
+        @1: @[@2, @4],
+        @2: @[@1, @3, @5],
+        @3: @[@2, @6],
+        @4: @[@1, @5, @7],
+        @5: @[@2, @4, @6, @8],
+        @6: @[@3, @5, @9],
+        @7: @[@4, @8],
+        @8: @[@5, @7, @9, @0],
+        @9: @[@6, @8],
+        @0: @[@8]
+    };
+    if ([number rangeOfCharacterFromSet: [[NSCharacterSet decimalDigitCharacterSet]invertedSet]].location != NSNotFound) return nil;
+    NSMutableSet <NSString *> *resultSet = [[NSMutableSet alloc] init];
+    NSRange range = NSMakeRange(0, 1);
+    for (int i=0; i<[number length]; i++) {
+        range.location = i;
+        
+        NSNumber *numberInDict = [[NSNumber alloc] initWithInt:[[number substringWithRange: range] intValue]];
+        
+        for (NSNumber *num in numberDict[numberInDict]) {
+            NSMutableString *tempString = [number mutableCopy];
+            NSMutableString *curNumber = [[NSMutableString alloc] initWithFormat:@"%d",[num intValue]];
+            [tempString replaceCharactersInRange:range withString:curNumber];
+            [resultSet addObject:tempString];
+        }
+    }
+    NSMutableArray <NSString *> *resultArray = [[NSMutableArray alloc] initWithArray:[resultSet allObjects]];
+    return resultArray;
 }
 @end
